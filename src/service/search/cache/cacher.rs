8cd1549490fa9042e7301054e617facb53ce82a6
oasisk <ashish.j.kolhe@gmail.com>
2024-07-16 11:06:34 +0530

    working fix

--- src/service/search/cache/cacher.rs
+++ src/service/search/cache/cacher.rs
@@ -125,7 +125,7 @@ pub async fn check_cache(
     }
     let query_key = file_path.replace('/', "_");
 
-    let mut is_descending = true;
+    let mut is_descending = false;
 
     if !order_by.is_empty() {
         for (field, order) in &order_by {
@@ -261,7 +261,7 @@ pub async fn get_cached_results(
                             cached_response.hits.last().unwrap(),
                         );
 
-                        let discard_ts = if first_ts > last_ts {
+                        let discard_ts = if cache_req.is_descending {
                             if cache_req.discard_interval > 0 {
                                 first_ts
                             } else {
