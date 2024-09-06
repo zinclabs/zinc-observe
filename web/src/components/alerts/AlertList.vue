e103381e1fa7b1e88b2cc21613d4584b394a875c
Sai Nikhil <nikhil@openobserve.ai>
2024-09-06 16:53:53 +0530

    added loader to the history table

--- web/src/components/alerts/AlertList.vue
+++ web/src/components/alerts/AlertList.vue
@@ -861,7 +861,6 @@ export default defineComponent({
   };
 
     const closeDialog = () => {
-      console.log("this is working")
      showHistoryDialog.value  = false;
       router.push({
         name: "alertList",
