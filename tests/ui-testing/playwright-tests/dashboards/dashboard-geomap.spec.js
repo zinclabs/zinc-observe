a2bfc3a9c4d5d8940940b39a27c76433ffc0f567
ktx-akshay <akshay.patil@kiara.tech>
2024-12-16 12:42:38 +0530

    Add the Geomap json data

---
+++ tests/ui-testing/playwright-tests/dashboards/dashboard-geomap.spec.js
@@ -0,0 +1,186 @@
+import { test, expect } from "../baseFixtures";
+import logData from "../../cypress/fixtures/log.json";
+import dashboard_geomap from "../../../test-data/dashboard_geomap.json";
+
+
+
+
+const randomDashboardName =
+  "Dashboard_" + Math.random().toString(36).slice(2, 11);
+
+test.describe.configure({ mode: "parallel" });
+
+async function login(page) {
+  await page.goto(process.env["ZO_BASE_URL"], { waitUntil: "networkidle" });
+  // await page.getByText('Login as internal user').click();
+  await page.waitForTimeout(1000);
+  await page
+    .locator('[data-cy="login-user-id"]')
+    .fill(process.env["ZO_ROOT_USER_EMAIL"]);
+
+  // wait for login api response
+  const waitForLogin = page.waitForResponse(
+    (response) =>
+      response.url().includes("/auth/login") && response.status() === 200
+  );
+
+  await page
+    .locator('[data-cy="login-password"]')
+    .fill(process.env["ZO_ROOT_USER_PASSWORD"]);
+  await page.locator('[data-cy="login-sign-in"]').click();
+
+  await waitForLogin;
+
+  await page.waitForURL(process.env["ZO_BASE_URL"] + "/web/", {
+    waitUntil: "networkidle",
+  });
+  await page
+    .locator('[data-test="navbar-organizations-select"]')
+    .getByText("arrow_drop_down")
+    .click();
+  await page.getByRole("option", { name: "default", exact: true }).click();
+}
+
+async function ingestion(page) {
+  const orgId = process.env["ORGNAME"];
+  const streamName = "geomap";
+  const basicAuthCredentials = Buffer.from(
+    `${process.env["ZO_ROOT_USER_EMAIL"]}:${process.env["ZO_ROOT_USER_PASSWORD"]}`
+  ).toString("base64");
+
+  const headers = {
+    Authorization: `Basic ${basicAuthCredentials}`,
+    "Content-Type": "application/json",
+  };
+  const fetchResponse = await fetch(
+    `${process.env.INGESTION_URL}/api/${orgId}/${streamName}/_json`,
+    {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(dashboard_geomap),
+    }
+  );
+  const response = await fetchResponse.json();
+  console.log(response);
+}
+
+async function waitForDashboardPage(page) {
+  const dashboardListApi = page.waitForResponse(
+    (response) =>
+      /\/api\/.+\/dashboards/.test(response.url()) && response.status() === 200
+  );
+
+  await page.waitForURL(process.env["ZO_BASE_URL"] + "/web/dashboards**");
+
+  await page.waitForSelector(`text="Please wait while loading dashboards..."`, {
+    state: "hidden",
+  });
+  await dashboardListApi;
+  await page.waitForTimeout(500);
+}
+
+test.describe("dashboard UI testcases", () => {
+  // let logData;
+  function removeUTFCharacters(text) {
+    // console.log(text, "tex");
+    // Remove UTF characters using regular expression
+    return text.replace(/[^\x00-\x7F]/g, " ");
+  }
+  async function applyQueryButton(page) {
+    // click on the run query button
+    // Type the value of a variable into an input field
+    const search = page.waitForResponse(logData.applyQuery);
+    await page.waitForTimeout(3000);
+    await page.locator("[data-test='logs-search-bar-refresh-btn']").click({
+      force: true,
+    });
+    // get the data from the search variable
+    await expect.poll(async () => (await search).status()).toBe(200);
+    // await search.hits.FIXME_should("be.an", "array");
+  }
+  // tebefore(async function () {
+  //   // logData("log");
+  //   // const data = page;
+  //   // logData = data;
+
+  //   console.log("--logData--", logData);
+  // });
+  test.beforeEach(async ({ page }) => {
+    console.log("running before each");
+    await login(page);
+    await page.waitForTimeout(1000);
+    await ingestion(page);
+    await page.waitForTimeout(2000);
+
+    // just to make sure org is set
+    const orgNavigation = page.goto(
+      `${logData.logsUrl}?org_identifier=${process.env["ORGNAME"]}`
+    );
+
+    await orgNavigation;
+  });
+
+
+  test("should correctly handle and display string and numeric values when no value replacement occurs", async ({
+    page,
+  }) => {
+    await page.locator('[data-test="menu-link-\\/dashboards-item"]').click();
+    await waitForDashboardPage(page);
+    await page.locator('[data-test="dashboard-add"]').click();
+    await page.locator('[data-test="add-dashboard-name"]').click();
+    await page
+      .locator('[data-test="add-dashboard-name"]')
+      .fill(randomDashboardName);
+    await page.locator('[data-test="dashboard-add-submit"]').click();
+
+    await page
+      .locator('[data-test="dashboard-if-no-panel-add-panel-btn"]')
+      .click();
+    await page
+      .locator("label")
+      .filter({ hasText: "Streamarrow_drop_down" })
+      .locator("i")
+      .click();
+    await page.getByRole("option", { name: "geomap" }).click();
+
+    await page
+      .locator(
+        '[data-test="field-list-item-logs-e2e_automate-kubernetes_annotations_kubectl_kubernetes_io_default_container"] [data-test="dashboard-add-y-data"]'
+      )
+      .click();
+    await page
+      .locator(
+        '[data-test="field-list-item-logs-e2e_automate-kubernetes_container_hash"] [data-test="dashboard-add-b-data"]'
+      )
+      .click();
+    await page.locator('[data-test="dashboard-apply"]').click();
+
+    await page.locator('[data-test="date-time-btn"]').click();
+    await page.locator('[data-test="date-time-relative-45-m-btn"]').click();
+    await page.locator(".layout-panel-container > .flex").click();
+    await page.getByText("expand_allConfig").click();
+    await page.locator(".q-pa-none > .q-list > div").first().click();
+    await page
+      .locator('[data-test="dashboard-config-no-value-replacement"]')
+      .click();
+    await page
+      .locator('[data-test="dashboard-config-no-value-replacement"]')
+      .fill("NA");
+    await page.locator('[data-test="dashboard-apply"]').click();
+
+    await page.locator('[data-test="dashboard-panel-name"]').click();
+    await page.locator('[data-test="dashboard-panel-name"]').fill("Dashboard");
+    await page.locator('[data-test="dashboard-panel-save"]').click();
+
+    await page
+      .locator('[data-test="dashboard-edit-panel-Dashboard-dropdown"]')
+      .click();
+    await page.locator('[data-test="dashboard-delete-panel"]').click();
+    await expect(page.getByText("Are you sure you want to")).toHaveText(
+      "Are you sure you want to delete this Panel?"
+    );
+    await page.locator('[data-test="confirm-button"]').click();
+  });
+
+
+});
