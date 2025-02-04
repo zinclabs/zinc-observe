ad5f48b1d649e105e221558ce955618eda3de450
sai nikhil kethe <nikhil@openobserve.ai>
2025-02-04 16:32:30 +0530

    fix: view panel renderer fixed

--- web/src/utils/dashboard/convertPanelData.ts
+++ web/src/utils/dashboard/convertPanelData.ts
@@ -123,6 +123,7 @@ export const convertPanelData = async (
 
       // Ensure result is an object
       const safeResult = result && typeof result === "object" ? result : {};
+      console.log(safeResult,'safe result')
       return {
         chartType: panelSchema.type,
         ...safeResult,
